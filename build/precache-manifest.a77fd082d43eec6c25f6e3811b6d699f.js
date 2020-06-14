19b1b5200093d796abdc1c4d9260d66b02a257a1
mickeyminion14 <sarthakagrawal6@gmail.com>
2020-06-14 17:22:47 +0530

    changes

---
+++ build/precache-manifest.a77fd082d43eec6c25f6e3811b6d699f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aacf1ee6c05ae644a04d9cf94534c346",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6d3c94b6453fbeef915",
+    "url": "/static/css/main.f3314537.chunk.css"
+  },
+  {
+    "revision": "5f9d47175f63c700a0c2",
+    "url": "/static/js/2.4e503fac.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/static/js/2.4e503fac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6d3c94b6453fbeef915",
+    "url": "/static/js/main.8b72d9d6.chunk.js"
+  },
+  {
+    "revision": "eecfd3db2c3ee7d5d724",
+    "url": "/static/js/runtime-main.411587af.js"
+  }
+]);
